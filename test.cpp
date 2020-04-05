8d24beee64f4d420ff594b817e29076667e4520b
eva97t <63176470+eva97t@users.noreply.github.com>
2020-04-05 13:22:44 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -8,35 +8,24 @@ class LPS {
 
 public:
   int findLPSLength(const string &st) {
-    vector<vector<int>> dp(st.length(), vector<int>(st.length(), -1));
-    return findLPSLengthRecursive(dp, st, 0, st.length() - 1);
-  }
-
-private:
-  int findLPSLengthRecursive(vector<vector<int>> &dp, const string &st, int startIndex,
-                             int endIndex) {
-    if (startIndex > endIndex) {
-      return 0;
-    }
-
+    vector<vector<int>> dp(st.length(), vector<int>(st.length(), 0));
     // every sequence with one element is a palindrome of length 1
-    if (startIndex == endIndex) {
-      return 1;
+    for (int i = 0; i < st.length(); i++) {
+      dp[i][i] = 1;
     }
 
-    if (dp[startIndex][endIndex] == -1) {
-      // case 1: elements at the beginning and the end are the same
-      if (st[startIndex] == st[endIndex]) {
-        dp[startIndex][endIndex] = 2 + findLPSLengthRecursive(dp, st, startIndex + 1, endIndex - 1);
-      } else {
-        // case 2: skip one element either from the beginning or the end
-        int c1 = findLPSLengthRecursive(dp, st, startIndex + 1, endIndex);
-        int c2 = findLPSLengthRecursive(dp, st, startIndex, endIndex - 1);
-        dp[startIndex][endIndex] = max(c1, c2);
+    for (int startIndex = st.length() - 1; startIndex >= 0; startIndex--) {
+      for (int endIndex = startIndex + 1; endIndex < st.length(); endIndex++) {
+        // case 1: elements at the beginning and the end are the same
+        if (st[startIndex] == st[endIndex]) {
+          dp[startIndex][endIndex] = 2 + dp[startIndex + 1][endIndex - 1];
+        } else { // case 2: skip one element either from the beginning or the end
+          dp[startIndex][endIndex] =
+              max(dp[startIndex + 1][endIndex], dp[startIndex][endIndex - 1]);
+        }
       }
     }
-    
-    return dp[startIndex][endIndex];
+    return dp[0][st.length() - 1];
   }
 };
 
