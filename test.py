2e4aadc3e7459e19ff22de7e5d8ad02963929e4a
eva97t <63176470+eva97t@users.noreply.github.com>
2020-04-05 12:45:31 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,28 @@
+def find_LPS_length(st):
+  n = len(st)
+  # dp[i][j] stores the length of LPS from index 'i' to index 'j'
+  dp = [[0 for _ in range(n)] for _ in range(n)]
 
-if st[endIndex] == st[startIndex] 
-  dp[startIndex][endIndex] = 2 + dp[startIndex + 1][endIndex - 1]
-else 
-  dp[startIndex][endIndex] = Math.max(dp[startIndex + 1][endIndex], dp[startIndex][endIndex - 1])
+  # every sequence with one element is a palindrome of length 1
+  for i in range(n):
+    dp[i][i] = 1
 
-  
+  for startIndex in range(n - 1, -1, -1):
+    for endIndex in range(startIndex + 1, n):
+      # case 1: elements at the beginning and the end are the same
+      if st[startIndex] == st[endIndex]:
+        dp[startIndex][endIndex] = 2 + dp[startIndex + 1][endIndex - 1]
+      else:  # case 2: skip one element either from the beginning or the end
+        dp[startIndex][endIndex] = max(
+          dp[startIndex + 1][endIndex], dp[startIndex][endIndex - 1])
+
+  return dp[0][n - 1]
+
+
+def main():
+  print(find_LPS_length("abdbca"))
+  print(find_LPS_length("cddpd"))
+  print(find_LPS_length("pqr"))
+
+
+main()
