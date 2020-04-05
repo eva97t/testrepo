38e7e1255330d0465fe8f88948885776c4c4c50c
eva97t <63176470+eva97t@users.noreply.github.com>
2020-04-04 20:22:14 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,16 +2,19 @@ using namespace std;
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 class LPS {
 
 public:
   int findLPSLength(const string &st) {
-    return findLPSLengthRecursive(st, 0, st.length() - 1);
+    vector<vector<int>> dp(st.length(), vector<int>(st.length(), -1));
+    return findLPSLengthRecursive(dp, st, 0, st.length() - 1);
   }
 
 private:
-  int findLPSLengthRecursive(const string &st, int startIndex, int endIndex) {
+  int findLPSLengthRecursive(vector<vector<int>> &dp, const string &st, int startIndex,
+                             int endIndex) {
     if (startIndex > endIndex) {
       return 0;
     }
@@ -21,15 +24,19 @@ private:
       return 1;
     }
 
-    // case 1: elements at the beginning and the end are the same
-    if (st[startIndex] == st[endIndex]) {
-      return 2 + findLPSLengthRecursive(st, startIndex + 1, endIndex - 1);
+    if (dp[startIndex][endIndex] == -1) {
+      // case 1: elements at the beginning and the end are the same
+      if (st[startIndex] == st[endIndex]) {
+        dp[startIndex][endIndex] = 2 + findLPSLengthRecursive(dp, st, startIndex + 1, endIndex - 1);
+      } else {
+        // case 2: skip one element either from the beginning or the end
+        int c1 = findLPSLengthRecursive(dp, st, startIndex + 1, endIndex);
+        int c2 = findLPSLengthRecursive(dp, st, startIndex, endIndex - 1);
+        dp[startIndex][endIndex] = max(c1, c2);
+      }
     }
-
-    // case 2: skip one element either from the beginning or the end
-    int c1 = findLPSLengthRecursive(st, startIndex + 1, endIndex);
-    int c2 = findLPSLengthRecursive(st, startIndex, endIndex - 1);
-    return max(c1, c2);
+    
+    return dp[startIndex][endIndex];
   }
 };
 
@@ -41,4 +48,3 @@ int main(int argc, char *argv[]) {
 
   delete lps;
 }
-
