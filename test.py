28075c9b7b9ebc04b3d3b80ac5e3854c9f4ed66f
eva97t <63176470+eva97t@users.noreply.github.com>
2020-04-05 19:39:59 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,31 @@
-Input: [3,3,5,0,0,3,1,4]
-Output: 6
-Explanation: 
-- Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
-- Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
-    
-    
-    
-Input: [1,2,3,4,5]
-Output: 4
-Explanation: 
-- Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
-- Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
-  engaging multiple transactions at the same time. You must sell before buying again.
-  
-  
-  
-Input: [7,6,4,3,1]
-Output: 0
-Explanation: 
-- In this case, no transaction is done, i.e. max profit = 0.
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) <= 1:
+            return 0
+
+        left_min = prices[0]
+        right_max = prices[-1]
+
+        length = len(prices)
+        left_profits = [0] * length
+        # pad the right DP array with an additional zero for convenience.
+        right_profits = [0] * (length + 1)
+
+        # construct the bidirectional DP array
+        for l in range(1, length):
+            left_profits[l] = max(left_profits[l-1], prices[l] - left_min)
+            left_min = min(left_min, prices[l])
+
+            r = length - 1 - l
+            right_profits[r] = max(right_profits[r+1], right_max - prices[r])
+            right_max = max(right_max, prices[r])
+
+        max_profit = 0
+        for i in range(0, length):
+            max_profit = max(max_profit, left_profits[i] + right_profits[i+1])
+
+        return max_profit
