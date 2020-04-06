3b86a83036e02add921f3bd915490c4c380004d0
eva97t <63176470+eva97t@users.noreply.github.com>
2020-04-05 19:44:42 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,29 +1,30 @@
+class Solution {
+  public:
+  int maxProfit(vector<int>& prices) {
+    int length = prices.size();
+    if (length <= 1) return 0;
 
-Input: [3,3,5,0,0,3,1,4]
-Output: 6
-    
-Explanation: 
-- Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
-- Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
-    
-    
-    
-Input: [1,2,3,4,5]
-Output: 4
-    
-Explanation: 
-- Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
-Note: 
-- You cannot buy on day 1, buy on day 2 and sell them later, as you are
-  engaging multiple transactions at the same time. 
-- You must sell before buying again.
-  
-  
-  
-Input: [7,6,4,3,1]
-Output: 0
-    
-Explanation: 
-- In this case, no transaction is done, i.e. max profit = 0.
-    
-    
+    int leftMin = prices[0];
+    int rightMax = prices[length - 1];
+
+    vector<int> leftProfits(length, 0);
+    // pad the right DP array with an additional zero for convenience.
+    vector<int> rightProfits(length + 1, 0);
+
+    // construct the bidirectional DP array
+    for (int l = 1; l < length; ++l) {
+      leftProfits[l] = max(leftProfits[l - 1], prices[l] - leftMin);
+      leftMin = min(leftMin, prices[l]);
+
+      int r = length - 1 - l;
+      rightProfits[r] = max(rightProfits[r + 1], rightMax - prices[r]);
+      rightMax = max(rightMax, prices[r]);
+    }
+
+    int maxProfit = 0;
+    for (int i = 0; i < length; ++i) {
+      maxProfit = max(maxProfit, leftProfits[i] + rightProfits[i + 1]);
+    }
+    return maxProfit;
+  }
+};
