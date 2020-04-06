a20c53c9bd851128ffbf07e480f7927a0da897b7
eva97t <63176470+eva97t@users.noreply.github.com>
2020-04-05 21:42:18 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,30 +1,20 @@
 class Solution {
   public:
   int maxProfit(vector<int>& prices) {
-    int length = prices.size();
-    if (length <= 1) return 0;
+    int t1Cost = INT_MAX, 
+        t2Cost = INT_MAX;
+    int t1Profit = 0,
+        t2Profit = 0;
 
-    int leftMin = prices[0];
-    int rightMax = prices[length - 1];
-
-    vector<int> leftProfits(length, 0);
-    // pad the right DP array with an additional zero for convenience.
-    vector<int> rightProfits(length + 1, 0);
-
-    // construct the bidirectional DP array
-    for (int l = 1; l < length; ++l) {
-      leftProfits[l] = max(leftProfits[l - 1], prices[l] - leftMin);
-      leftMin = min(leftMin, prices[l]);
-
-      int r = length - 1 - l;
-      rightProfits[r] = max(rightProfits[r + 1], rightMax - prices[r]);
-      rightMax = max(rightMax, prices[r]);
+    for (int price : prices) {
+      // the maximum profit if only one transaction is allowed
+      t1Cost = min(t1Cost, price);
+      t1Profit = max(t1Profit, price - t1Cost);
+      // re-invest the gained profit in the second transaction
+      t2Cost = min(t2Cost, price - t1Profit);
+      t2Profit = max(t2Profit, price - t2Cost);
     }
 
-    int maxProfit = 0;
-    for (int i = 0; i < length; ++i) {
-      maxProfit = max(maxProfit, leftProfits[i] + rightProfits[i + 1]);
-    }
-    return maxProfit;
+    return t2Profit;
   }
 };
